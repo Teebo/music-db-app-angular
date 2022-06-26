b0eeb18a66cf44ba0268ff67fb347e630ef5b433
Thabo <ngubanethabo.ambrose@gmail.com>
2022-06-26 13:07:56 +0200

    Add artists view feature module

---
+++ src/app/modules/artists-view/artists-view.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArtistsViewRoutingModule } from './artists-view-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ArtistsViewRoutingModule
+  ]
+})
+export class ArtistsViewModule { }
