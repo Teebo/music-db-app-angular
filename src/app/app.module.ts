c2fe122c46562d5b8598d052f8057d2038bb8272
Thabo <ngubanethabo.ambrose@gmail.com>
2022-06-26 13:04:02 +0200

    Import UI modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const MatModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+]
 
 @NgModule({
   declarations: [
@@ -12,9 +22,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    ...MatModules
   ],
   providers: [],
+  exports: [MatModules],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
