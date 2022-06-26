0ab933e457c28bae24470cc545eca32d40606377
Thabo <ngubanethabo.ambrose@gmail.com>
2022-06-26 13:08:42 +0200

    Add artist detail view feature module

---
+++ src/app/modules/artist-detail-view/artist-detail-view.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArtistDetailViewRoutingModule } from './artist-detail-view-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ArtistDetailViewRoutingModule
+  ]
+})
+export class ArtistDetailViewModule { }
