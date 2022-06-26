d57eb80d0c8373571fe2fa439321264b0bebb378
Thabo <ngubanethabo.ambrose@gmail.com>
2022-06-26 13:11:02 +0200

    Add default pages for feature modules

--- src/app/modules/artist-detail-view/artist-detail-view.module.ts
+++ src/app/modules/artist-detail-view/artist-detail-view.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ArtistDetailViewRoutingModule } from './artist-detail-view-routing.module';
+import { DefaultComponent } from './pages/default/default.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DefaultComponent
+  ],
   imports: [
     CommonModule,
     ArtistDetailViewRoutingModule
--- src/app/modules/artists-view/artists-view.module.ts
+++ src/app/modules/artists-view/artists-view.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ArtistsViewRoutingModule } from './artists-view-routing.module';
+import { DefaultComponent } from './pages/default/default.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DefaultComponent
+  ],
   imports: [
     CommonModule,
     ArtistsViewRoutingModule
